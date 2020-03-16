15743cdb29205e0036d8d836e2cb9086ec790184
Nasembele <50178818+Nasembele@users.noreply.github.com>
2020-03-16 22:03:46 +0300

    Add files via upload

--- srcs/algorithm.c
+++ srcs/algorithm.c
@@ -6,7 +6,7 @@
 /*   By: soyster <soyster@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/16 19:10:52 by soyster           #+#    #+#             */
-/*   Updated: 2020/03/16 19:14:26 by soyster          ###   ########.fr       */
+/*   Updated: 2020/03/16 20:54:23 by soyster          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,22 @@
 
 int ft_algorithm(t_map *map, t_piece *pic)
 {
-        
+    int j;
+    int i;
+
+    j = 0;
+    while (j < map->height)
+    {
+        i = 0;
+        while (i < map->height)
+        {
+            if (j + pic->height <= map->height && i + pic->width <= map->width)
+            {
+                
+                
+            }
+            i++;
+        }
+        j++;
+    }      
 }
